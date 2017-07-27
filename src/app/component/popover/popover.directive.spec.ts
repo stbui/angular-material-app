1ee56694d5919aea73103f0cc4e4fabcc21291a0
stbui <w431106@163.com>
2017-07-27 11:29:23 +0800

    delete *.spec.ts

--- src/app/component/popover/popover.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PopoverDirective } from './popover.directive';
-
-describe('PopoverDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PopoverDirective();
-    expect(directive).toBeTruthy();
-  });
-});
