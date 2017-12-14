3a188be4ed27bb31fe19a3179c4df78249891e64
stbui <w431106@163.com>
2017-12-14 21:00:04 +0800

    update home page verison

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
-
 }
--- src/app/home/home.routing.ts
+++ src/app/home/home.routing.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home.component';
