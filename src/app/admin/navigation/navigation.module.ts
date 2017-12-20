2cbde75a9e8ad762bca0e3db675ec0e3d355713f
stbui <w431106@163.com>
2017-12-20 09:36:26 +0800

    navigation add ngx-pefect-scrollbar

--- src/app/admin/navigation/navigation.module.ts
+++ src/app/admin/navigation/navigation.module.ts
@@ -7,15 +7,29 @@ import { NavCollapseComponent } from './nav-collapse/nav-collapse.component';
 import { NavItemComponent } from './nav-item/nav-item.component';
 import { NavigationService } from './navigation.service';
 
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
+
 @NgModule({
   imports: [
     CommonModule,
     MatIconModule,
     MatRippleModule,
-    RouterModule
+    RouterModule,
+    PerfectScrollbarModule
   ],
   declarations: [NavigationComponent, NavCollapseComponent, NavItemComponent],
-  providers: [NavigationService],
+  providers: [
+    NavigationService,
+    {
+      provide: PERFECT_SCROLLBAR_CONFIG,
+      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
+    }],
   exports: [NavigationComponent]
 })
 export class NavigationModule {
