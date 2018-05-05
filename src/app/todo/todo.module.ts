5e3c93ed9d182272628c50d297f8447c793ffda0
stbui <w431106@163.com>
2018-05-05 15:53:32 +0800

    style: 删除todo-search

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -4,16 +4,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgxDnDModule } from '@swimlane/ngx-dnd';
 
 import { SearchModule } from '../component';
+
 import { TodoRoutingModule } from './todo.routing';
 import { TodoComponent } from './todo.component';
 import { TodoService } from './todo.service';
 import { TodoHeaderComponent } from './todo-header/todo-header.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
-
 import { TodoSidenavComponent } from './todo-sidenav/todo-sidenav.component';
 import { TodoToolbarComponent } from './todo-toolbar/todo-toolbar.component';
-import { TodoSearchComponent } from './todo-search/todo-search.component';
 
 @NgModule({
   imports: [
@@ -29,12 +28,8 @@ import { TodoSearchComponent } from './todo-search/todo-search.component';
     TodoItemComponent,
     TodoListComponent,
     TodoSidenavComponent,
-    TodoToolbarComponent,
-    TodoSearchComponent
+    TodoToolbarComponent
   ],
-  providers: [
-    {provide: 'todoService', useClass: TodoService}
-  ]
+  providers: [{ provide: 'todoService', useClass: TodoService }]
 })
-export class TodoModule {
-}
+export class TodoModule {}
