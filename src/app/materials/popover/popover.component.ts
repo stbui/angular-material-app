cdf8d80ff4f62eb677f6c5cbc64f9e6f10f304b5
stbui <w431106@163.com>
2018-05-06 21:54:00 +0800

    style: demo

--- src/app/materials/popover/popover.component.ts
+++ src/app/materials/popover/popover.component.ts
@@ -6,11 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./popover.component.scss']
 })
 export class PopoverComponent implements OnInit {
+  horizontalAlign = 'after';
+  verticalAlign = 'center';
+  margin = 0;
+  forceAlignment = false;
+  lockAlignment = false;
+
+  strategy = 'reposition';
+  scrollOptions = [
+    { value: 'noop', name: 'Do nothing' },
+    { value: 'block', name: 'Block scrolling' },
+    { value: 'reposition', name: 'Reposition on scroll' },
+    { value: 'close', name: 'Close on scroll' },
+    { value: 'rugrats', name: 'Invalid option' }
+  ];
+
   constructor() {}
 
   ngOnInit() {}
-
-  test(popover) {
-    console.log(popover)
-  }
 }
