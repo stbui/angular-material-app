ea1f381cc581d392a7b496e8a4e3a8a24269fe64
stbui <w431106@163.com>
2018-06-02 12:34:43 +0800

    style: upate base

--- src/app/todo/todo-header/todo-header.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-todo-header',
-  templateUrl: './todo-header.component.html',
-  styleUrls: ['./todo-header.component.scss']
-})
-export class TodoHeaderComponent {}
