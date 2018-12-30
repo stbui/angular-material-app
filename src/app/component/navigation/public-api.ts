3b713707ccfced47d5bceac5848fd9116638817d
stbui <w431106@163.com>
2018-12-30 22:25:47 +0800

    refactor(admin): 拆分组件

---
+++ src/app/component/navigation/public-api.ts
@@ -0,0 +1,10 @@
+/**
+ * @license
+ * Copyright Stbui All Rights Reserved.
+ * https://github.com/stbui
+ */
+
+export * from './navigation.module';
+export * from './navigation.service';
+export * from './navigation.component';
+export * from './navigation.animation';
