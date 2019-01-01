e1e26ea2468d8138603701628f37f47bb4ab793e
stbui <w431106@163.com>
2019-01-01 22:47:01 +0800

    feat(mail): 邮件列表优化

--- src/app/mail/list/list.component.ts
+++ src/app/mail/list/list.component.ts
@@ -1,4 +1,13 @@
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnInit,
+  ViewChild
+} from '@angular/core';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { SelectionModel, DataSource } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-mail-list',
@@ -6,14 +15,36 @@ import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-  @Input() mails;
+  displayedColumns: string[] = ['select', 'star', 'name', 'subject', 'when'];
+  dataSource: any = new MatTableDataSource([]);
+  selection = new SelectionModel(true, []);
+
+  @Input()
+  set mails(val) {
+    this.dataSource = new MatTableDataSource(val);
+  }
   @Output() onOpenMailDetial = new EventEmitter();
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor() {}
 
   ngOnInit() {}
 
-  onOpenMailDetialTriggered(mail) {
-    this.onOpenMailDetial.emit(mail);
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  masterToggle() {
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  rowSelection(row) {
+    console.log(row);
+    this.onOpenMailDetial.emit(row);
   }
 }
