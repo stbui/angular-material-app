985301e95f4776b9fd78d9426cdd91d7835c78ef
stbui <w431106@163.com>
2019-01-18 23:11:20 +0800

    fix(admin): 使用MediaObserver订阅

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { MediaChange, ObservableMedia } from '@angular/flex-layout';
-import { Observable, ReplaySubject, Subscription } from 'rxjs';
+import { MediaObserver } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
 import { ConfigService } from '../core/config.service';
 import { NavigationService } from '../component/navigation';
 
@@ -19,25 +19,16 @@ export class AdminComponent implements OnInit, OnDestroy {
   onSettingsChanged: Subscription;
   layoutMode: boolean = false;
 
-  navigationModel;
+  navigationModel: any;
   navigationModelChangeSubscription: Subscription;
-
-  private _media$: ReplaySubject<MediaChange> = new ReplaySubject(1);
-  private _mediaSubscription: Subscription;
 
   sidenavOpen: boolean = true;
   sidenavMode: string = 'side';
   sidenavAlign: string = 'start';
   customizerSidenavAlign: string = 'end';
 
-  title = '中后台前端应用框架 - Power by stbui';
-
-  get media$(): Observable<MediaChange> {
-    return this._media$.asObservable();
-  }
-
   constructor(
-    media: ObservableMedia,
+    private mediaObserver: MediaObserver,
     private config: ConfigService,
     private translateService: TranslateService,
     private translationService: TranslationService,
@@ -48,14 +39,6 @@ export class AdminComponent implements OnInit, OnDestroy {
         this.navigationModel = navigation;
       }
     );
-
-    media
-      .asObservable()
-      .subscribe(
-        res => this._media$.next(res),
-        err => this._media$.error(err),
-        () => this._media$.complete()
-      );
 
     this.onSettingsChanged = this.config.onSettingsChanged.subscribe(
       settings => {
@@ -88,16 +71,11 @@ export class AdminComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this._mediaSubscription = this.media$.subscribe((change: MediaChange) => {
+    this.mediaObserver.media$.subscribe(change => {
       let isMobile = change.mqAlias === 'xs' || change.mqAlias === 'sm';
-
       this.sidenavMode = isMobile ? 'over' : 'side';
       this.sidenavOpen = !isMobile;
     });
-
-    setTimeout(() => {
-      window.dispatchEvent(new Event('resize'));
-    }, 2000);
   }
 
   ngOnDestroy() {
